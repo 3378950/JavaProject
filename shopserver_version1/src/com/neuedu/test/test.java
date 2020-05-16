eb46fd856fc1a6053b96d58cfb59e81d0840462a
3378950 <374008063@qq.com>
2020-05-16 11:19:31 +0800

    shopserver version1

---
+++ shopserver_version1/src/com/neuedu/test/test.java
@@ -0,0 +1,14 @@
+package com.neuedu.test;
+
+import com.neuedu.dao.impl.ProductDaoImpl;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ProductDaoImpl tmp = new ProductDaoImpl();
+		tmp.ListProductByLimit();
+		
+	}
+
+}
